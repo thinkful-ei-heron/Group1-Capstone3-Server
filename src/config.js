80d30c651068b29deaab3aa3a8931375d5f8e44a
Nathan Leung <nath.leung@gmail.com>
2020-01-06 20:10:04 -0800

    updates to router, service, jwt-auth and config

--- src/config.js
+++ src/config.js
@@ -2,4 +2,7 @@ module.exports = {
     NODE_ENV : process.env.NODE_ENV || 'development',
     PORT : process.env.PORT || 8000,
     DB_URL: process.env.DB_URL
+      || 'postgresql://',
+    JWT_SECRET: process.env.JWT_SECRET || '',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '',
 };
