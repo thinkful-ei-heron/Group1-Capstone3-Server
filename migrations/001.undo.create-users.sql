7d1deec89117ca61bb90cb84664ac1c3cbfcf90b
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-09 10:23:34 -0700

    small database change

--- migrations/001.undo.create-users.sql
+++ migrations/001.undo.create-users.sql
@@ -1,5 +1,8 @@
 begin;
+drop table if exists game_data;
 drop table if exists game_history;
+drop type if exists gameStatus;
+drop type if exists playerTurn;
 drop table if exists stats;
 drop table if exists users;
 commit;
