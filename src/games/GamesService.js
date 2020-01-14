bb03594b3f8b9a5f89d67d8019f79457e68a299a
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-14 12:46:33 -0700

    win conditions functioning, updating game_data, game_history, and stats working in the database

--- src/games/GamesService.js
+++ src/games/GamesService.js
@@ -26,7 +26,63 @@ const GamesService = {
       .then(rows => {
         return rows;
       });
-  }
+  },
+
+
+  getPlayerIds(db, game_id){
+    return db
+    .from('game_history')
+    .select('game_history.player1', 'game_history.player2')
+    .where({id: game_id})
+    .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  updateGameData(db, game_id, winner){
+    return db
+    .from('game_data')
+    .where({game_id})
+    .update({winner})
+    .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  endGame(db, game_id){
+    return db
+      .from('game_history')
+      .where({id: game_id})
+      .update({game_status: 'complete'})
+      .returning('*')
+      .then(rows => {
+        return rows[0];
+      });
+  },
+
+  updateWinnerStats(db, winner_id){
+    return db
+    .from('stats')
+    .where({userid: winner_id})
+    .increment('wins', 1)
+    .returning('*')
+    .then(rows => {
+      return rows[0];
+    });
+  },
+
+  updateLoserStats(db, loser_id){
+    return db
+    .from('stats')
+    .where({userid: loser_id})
+    .increment('losses', 1)
+    .returning('*')
+    .then(rows => {
+      return rows[0];
+    });
+  },
 
 };
 
