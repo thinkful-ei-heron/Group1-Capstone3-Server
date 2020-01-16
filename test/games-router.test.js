8c35a440352ad5d1fb276255da8efc3b5e50664c
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-01-16 16:56:11 -0700

    all tests still passing

--- test/games-router.test.js
+++ test/games-router.test.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
 
-describe.only('Games Endpoints', () => {
+describe('Games Endpoints', () => {
   let db;
   const {
     testUsers,
