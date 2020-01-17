9244cbee94c39978f3907ae1bf1d98059a55a926
Just-A-Fool <aedanscarn@gmail.com>
2020-01-17 15:07:25 -0700

    added new routes and tests for said routes.

--- src/games/games-router.js
+++ src/games/games-router.js
@@ -6,32 +6,66 @@ const GamesService = require('./GamesService');
 gamesRouter
   //this endpoint retrives all of the logged in user's active games
   .route('/')
+  .get(async (req, res, next) => {
+    const db = req.app.get('db');
+    const userId = req.app.get('user').id;
+
+    //Checks for any games that have expired
+    let expiredGames = await GamesService.getExpiredGames(db, userId);
+
+    //If any games have expired
+    if (expiredGames.length) {
+      await expiredGames.forEach(async game => {
+        winnerString = game.turn === 'player1' ? 'player2' : 'player1';
+        winnerId = game.turn === 'player1' ? game.player2 : game.player1;
+        loserId = game.turn === 'player1' ? game.player1 : game.player2;
+
+        //Make calls to db to update expired games and both players stats
+        await Promise.all([
+          GamesService.updateGameDataWin(db, game.game_data_id, winnerString),
+          GamesService.expireGame(db, game.game_id),
+          GamesService.updateWinnerStats(db, winnerId),
+          GamesService.updateLoserStats(db, loserId)
+        ]);
+      });
+
+    }
+
+    //Returns only games that are active
+    let activeGames = await GamesService.getAllActiveGames(db, userId);
+    return res.status(200).json({ result: activeGames, userId });
+
+  });
+
+
+gamesRouter
+  .route('/prev')
+  .get(async (req, res, next) => {
+    const db = req.app.get('db');
+    const userId = req.app.get('user').id;
+
+    let previousGames = await GamesService.getAllPreviousGames(db, userId);
+    return res.status(200).json({result: previousGames, userId});
+
+  })
+
+//this endpoint retreives the logged in user's game stats
+gamesRouter
+  .route('/stats')
   .get((req, res, next) => {
     const knexInstance = req.app.get('db');
     const userId = req.app.get('user').id;
-    GamesService.getAllActiveGames(knexInstance, userId).then(result => {
-      return res.status(200).json({ result, userId });
-    })
-    .catch(next);
-  });
-
-  //this endpoint retreives the logged in user's game stats
-gamesRouter
-  .route('/stats')
-  .get((req,res, next) => {
-    const knexInstance = req.app.get('db');
-    const userId = req.app.get('user').id;
     GamesService.getUserStats(knexInstance, userId)
-    .then(stats => {
-      res.status(200).json(stats);
-    })
-    .catch(next);
+      .then(stats => {
+        res.status(200).json(stats);
+      })
+      .catch(next);
   });
 
-  //this endpoint retrieves the game data for a specific game based on the 
-  //gameId sent in the params. Of note, if the opponent ships are sent we will
-  // return 'true' otherwise we will return 'false'. This prevents us from sending 
-  // the location data for the opponent's ships to the current user.
+//this endpoint retrieves the game data for a specific game based on the 
+//gameId sent in the params. Of note, if the opponent ships are sent we will
+// return 'true' otherwise we will return 'false'. This prevents us from sending 
+// the location data for the opponent's ships to the current user.
 gamesRouter
   .route('/activegame/:gameId/:playerNum')
   .get((req, res, next) => {
@@ -40,22 +74,22 @@ gamesRouter
     let opponentString = (playerNum === 'player1') ? 'player2' : 'player1';
     let playerString = playerNum;
 
-    if(isNaN(gameId)){
-      return res.status(400).json({error: 'Must send a valid game id'});
+    if (isNaN(gameId)) {
+      return res.status(400).json({ error: 'Must send a valid game id' });
     }
 
-    if(playerNum !== 'player1' && playerNum !== 'player2'){
-      return res.status(400).json({error: 'Must send a valid playerNum'});
+    if (playerNum !== 'player1' && playerNum !== 'player2') {
+      return res.status(400).json({ error: 'Must send a valid playerNum' });
     }
 
 
     GamesService.retrieveGameData(knexInstance, gameId).then(data => {
       let gameData = data;
-      if(!gameData){
-        return res.status(400).json({error: 'Must send a gameId of an existing game'});
+      if (!gameData) {
+        return res.status(400).json({ error: 'Must send a gameId of an existing game' });
       }
-      if(gameData.winner){
-        return res.status(400).json({error: 'Cannot resume a completed game'});
+      if (gameData.winner) {
+        return res.status(400).json({ error: 'Cannot resume a completed game' });
       }
 
       gameData[`${opponentString}_ships`] = gameData[`${opponentString}_ships`] ? true : false;
@@ -66,29 +100,29 @@ gamesRouter
       gameData.player2_misses = JSON.parse(gameData.player2_misses);
       gameData.currentUser = playerNum;
 
-      res.status(200).json(gameData); 
+      res.status(200).json(gameData);
     })
-    .catch(next);
+      .catch(next);
   });
 
-  //this endpoint retrieves all of the data from the game_data table for the finished game.
-  gamesRouter
+//this endpoint retrieves all of the data from the game_data table for the finished game.
+gamesRouter
   .route('/results/:gameId')
   .get((req, res, next) => {
     const knexInstance = req.app.get('db');
-    const  { gameId }  = req.params;
-    GamesService.retrieveResults(knexInstance,  gameId).then(data => {
-      if(!data[0]){
-        return res.status(400).json({error: 'Must send a gameId of an existing game'});
-      } 
-      
-      if(!data[0].winner){
-        return res.status(400).json({error: 'Game is not completed'});
+    const { gameId } = req.params;
+    GamesService.retrieveResults(knexInstance, gameId).then(data => {
+      if (!data[0]) {
+        return res.status(400).json({ error: 'Must send a gameId of an existing game' });
+      }
+
+      if (!data[0].winner) {
+        return res.status(400).json({ error: 'Game is not completed' });
       }
       res.status(200).json(data);
-  })
-  .catch(next);
-});
+    })
+      .catch(next);
+  });
 
 
 
